bb6800627f65971e94b50ec14675db4daa84f0f3
gospodinovkris4-ai <gospodinovkris4@gmail.com>
2026-01-30 02:07:27 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://aujswymolniwtdxyangf.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_UNykLLkp5Jzb9TQlq-qijg_U94miJxS";
+
+  window.supabase = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+</script>
